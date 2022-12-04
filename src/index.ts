96f4c00fe631186b252142a0a2391ec451b09fa1
Lukas Tietze <lukas.tietzermp@gmail.com>
2022-12-04 17:07:05 +0100

    Baut

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+alert('Skript läuft!')
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,61 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './dist/main.js',
-  mode: 'development',
+  entry: {
+    index: ['./src/index.ts', './src/index.scss'],
+  },
+  output: {
+    filename: '[name].[contenthash].bundle.js',
+    path: './dist',
+    clean: true,
+  },
+  mode: 'production',
   module: {
     rules: [
       {
-        test: /\.js?$/,
+        // html-Dateien (außer bei Komponenten).
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {},
+        exclude: [/node_modules/],
+      },
+      {
+        // TypeScript-Dateien.
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        // "normale" scss-Stylesheets
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: false,
+            },
+          },
+          'sass-loader',
+        ],
+        exclude: [/node_modules/],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      chunks: ['index'],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].bundle.css',
+    }),
+  ],
   resolve: {
-    extensions: ['.js'],
+    extensions: ['.js', '.ts'],
   },
   output: {
     filename: 'bundle.js',
