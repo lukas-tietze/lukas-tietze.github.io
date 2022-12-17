d3d19ff2eee88ce63401be52e6b2d8e4c1b17694
Lukas Tietze <lukas.tietzermp@gmail.com>
2022-12-17 13:48:29 +0100

    Asset-Umabeu

--- webpack/loaders/html-template-loader.js
+++ webpack/loaders/html-template-loader.js
@@ -1,6 +1,25 @@
 const path = require('path');
-const fs = require('fs/promises');
-const glob = require('glob-all');
+
+async function evalInContext(js, context) {
+  return async function () {
+    return String(await eval(js));
+  }.call(context);
+}
+
+function applyPipe(val, pipe) {
+  if (!pipe) {
+    return val;
+  }
+
+  switch (pipe) {
+    case 'date':
+      return new Intl.DateTimeFormat('de', {
+        dateStyle: 'medium',
+      }).format(new Date(val));
+    default:
+      throw new Error('Unknown Pipe: ', pipe);
+  }
+}
 
 /**
  * @param {string} content
@@ -8,20 +27,31 @@ const glob = require('glob-all');
 module.exports = async function (content, map, meta) {
   const callback = this.async();
   const partialDir = path.resolve('');
-  const regex = /{{\s*(?<partial>\w+)\s*}}/g;
+  const regex = /{{(?<mode>>|!)(?<exp>[^\|}]+)(\|(?<pipe>.+))?}}/g;
   let match;
-  let file;
+  let mode;
+  let exp;
   let replaced = 0;
   let newContent = '';
 
-  while ((match = regex.exec(content)) && (file = match.groups['partial'])) {
-    const data = await new Promise((resolve, reject) => {
-      this.loadModule(path.resolve(`src/partials/${file}.partial.html`), (err, src, srcMap, module) => (err ? reject(err) : resolve(src)));
-    });
+  while ((match = regex.exec(content)) && (mode = match.groups['mode']) && (exp = match.groups['exp']?.trim())) {
+    let replacedExpression = '';
 
-    const html = data.toString('utf8');
+    console.log(mode, exp);
 
-    newContent += content.substring(replaced, match.index) + html;
+    if (mode === '>') {
+      const data = await new Promise((resolve, reject) => {
+        this.loadModule(path.resolve(`src/partials/${exp}.partial.html`), (err, src, _srcMap, _module) =>
+          err ? reject(err) : resolve(src)
+        );
+      });
+
+      replacedExpression = data.toString('utf8');
+    } else if (mode === '!') {
+      replacedExpression = applyPipe(await evalInContext(exp, {}), match.groups['pipe']?.trim());
+    }
+
+    newContent += content.substring(replaced, match.index) + replacedExpression;
     replaced = match.index + match[0].length;
   }
 
